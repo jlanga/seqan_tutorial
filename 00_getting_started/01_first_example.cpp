00fd2007c0ab78caa06025a209eb418e3411cd7f
Jorge Langa <jorge.langa.arranz@gmail.com>
2018-07-20 18:26:28 +0200

    Added templates

--- 00_getting_started/01_first_example.cpp
+++ 00_getting_started/01_first_example.cpp
@@ -1,54 +1,45 @@
 #include <iostream>
 #include <seqan/file.h>
 #include <seqan/sequence.h>
+#include <seqan/score.h>
 
 using namespace seqan;
 
-int computeLocalScore(String<char> const & subText, String<char> const & pattern)
+template <typename TText, typename TPattern>
+int computeLocalScore(TText const & subText, TPattern const & pattern)
 {
     int localScore = 0;
     for (unsigned i = 0; i < length(pattern); ++i)
         if (subText[i] == pattern[i])
             ++localScore;
-    
+
     return localScore;
 }
 
-
-String<int> computeScore(String<char> const & text, String<char> const & pattern)
+template <typename TText, typename TPattern>
+String<int> computeScore(TText const & text, TPattern const & pattern)
 {
     String<int> score;
-    resize(score, length(text) - length(pattern) + 1);
-    
-    for (unsigned i = 0; i < length(score); ++i){
+    resize(score, length(text) - length(pattern) + 1, 0);
+
+    for (unsigned i = 0; i < length(text) - length(pattern) + 1; ++i)
         score[i] = computeLocalScore(infix(text, i, i + length(pattern)), pattern);
-    }
-    
+
     return score;
 }
 
-
-
-void print(String<int> const & score)
+void print(String<int> const & text)
 {
-    for (unsigned i = 0; i < length(score); ++i)
-        std::cout << score[i] << " ";
+    for (unsigned i = 0; i < length(text); ++i)
+        std::cout << text[i] << " ";
     std::cout << std::endl;
-    
 }
 
 int main()
 {
-    // Initialize
     String<char> text = "This is an awesome tutorial to get to know SeqAn!";
     String<char> pattern = "tutorial";
-    
-    // Compute
     String<int> score = computeScore(text, pattern);
-    
-    
-    // Print the results
-    printScore(score);
-    
+    print(score);
     return 0;
 }
