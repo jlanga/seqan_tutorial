a06ae0bf26c1d20653590d6cb16cf83ae44cfed7
Jorge Langa <jorge.langa.arranz@gmail.com>
2018-07-20 18:37:12 +0200

    max only and greater than zero

--- 00_getting_started/01_first_example.cpp
+++ 00_getting_started/01_first_example.cpp
@@ -5,17 +5,15 @@
 
 using namespace seqan;
 
-
 template <typename TText>
-int computeLocalScore(TText const & subText, seqan::String<seqan::AminoAcid> const & pattern)
+int computeLocalScore(TText const & subText, String<AminoAcid> const & pattern)
 {
     int localScore = 0;
-    for (unsigned i = 0; i < seqan::length(pattern); ++i)
-        localScore += score(seqan::Blosum62(), subText[i], pattern[i]);
+    for (unsigned i = 0; i < length(pattern); ++i)
+        localScore += score(Blosum62(), subText[i], pattern[i]);
 
     return localScore;
 }
-
 
 template <typename TText, typename TPattern>
 int computeLocalScore(TText const & subText, TPattern const & pattern)
@@ -27,8 +25,6 @@ int computeLocalScore(TText const & subText, TPattern const & pattern)
 
     return localScore;
 }
-
-
 
 template <typename TText, typename TPattern>
 String<int> computeScore(TText const & text, TPattern const & pattern)
@@ -48,11 +44,54 @@ void print(TText const & text)
     std::cout << text << std::endl;
 }
 
-
 void print(String<int> const & text)
 {
     for (unsigned i = 0; i < length(text); ++i)
         std::cout << text[i] << " ";
+    std::cout << std::endl;
+}
+
+template <typename TText, typename TSpec>
+void print(TText const & text, TSpec const & /*tag*/)
+{
+    print(text);
+}
+
+struct MaxOnly {};
+
+template <typename TText>
+void print(TText const & score, MaxOnly const & /*tag*/)
+{
+    int maxScore = score[0];
+    String<int> output;
+    appendValue(output, 0);
+    for (unsigned i = 1; i < length(score); ++i)
+    {
+        if (score[i] > maxScore)
+        {
+            maxScore = score[i];
+            clear(output);
+            resize(output, 1, i);
+        }
+        else if (score[i] == maxScore)
+            appendValue(output, i);
+    }
+
+    print(output);
+}
+
+struct GreaterZero {};
+
+template <typename TText>
+void print(TText const & score, GreaterZero const & /*tag*/)
+{
+    String<Pair<int> > output;
+    for (unsigned i = 1; i < length(score); ++i)
+        if (score[i] > 0)
+            appendValue(output, Pair<int>(i, score[i]));
+
+    for (unsigned i = 0; i < length(output); ++i)
+        std::cout << "(" << output[i].i1 << "; " << output[i].i2 << ") ";
     std::cout << std::endl;
 }
 
@@ -61,10 +100,31 @@ int main()
     String<char> text = "This is an awesome tutorial to get to know SeqAn!";
     String<char> pattern = "tutorial";
     String<int> score = computeScore(text, pattern);
-    
+
     print(text);
+
     print(pattern);
+
     print(score);
-    
+
+    print(score, MaxOnly());
+
+    print(score, GreaterZero());
+
+    // And now for a protein pattern
+    String<AminoAcid> protein = "tutorial";
+    String<int> proteinScore = computeScore(text, protein);
+
+    print(text);
+
+    print(protein);
+
+    print(proteinScore);
+
+    print(proteinScore, MaxOnly());
+
+    print(proteinScore, GreaterZero());
+
     return 0;
 }
+
